1884d82b45880b5cde33010cfc643c46a31e804e
mullenlucas <mullenlucas@gmail.com>
2022-11-07 20:04:11 -0300

    add person class

---
+++ entities/person.rb
@@ -0,0 +1,26 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+  attr_writer :parent_permission
+
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    @parent_permission || is_of_age?
+  end
+
+  def is_of_age?
+    @age >= 18
+  end
+end
+
+# testing
+person = Person.new(24, "David", true)
+puts person.can_use_services? # true
+puts person.name
+puts person.age
