e636f013dc2818c146a3b020404cb4b4d5e545aa
mullenlucas <mullenlucas@gmail.com>
2022-11-08 16:11:30 -0300

    make Person class inherit from Nameable

--- entities/person.rb
+++ entities/person.rb
@@ -1,9 +1,11 @@
-class Person
+require_relative '../operations/nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
   attr_writer :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
