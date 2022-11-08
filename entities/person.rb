8b5b71730a9f36ea1b6846f649d569f30fe03dd8
mullenlucas <mullenlucas@gmail.com>
2022-11-07 21:53:42 -0300

    fix rubocop offenses

--- entities/person.rb
+++ entities/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :name, :age
   attr_writer :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,12 +11,12 @@ class Person
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?
+    @parent_permission || of_age?
   end
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
