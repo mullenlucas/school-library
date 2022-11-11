639200a2dbe1471780984eb453dc3072c369a47a
mullenlucas <mullenlucas@gmail.com>
2022-11-11 00:18:37 -0300

    add create_rental and list_rentals to app

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App
   def run
     user_input = 0
     puts "####\nWelcome to the School Library Ruby App!\n####"
-    while user_input != '5'
+    while user_input != '7'
       puts "Choose a number from the options below:\n"
       app_options
       print 'Enter Option [number]: '
@@ -21,7 +21,7 @@ class App
       puts "\n\n"
       check_selection(user_input)
     end
-    puts "Exiting, thanks for using this app!\n\n" if user_input == '5'
+    puts "Exiting, thanks for using this app!\n\n" if user_input == '7'
   end
 
   def list_all_books
@@ -76,6 +76,45 @@ class App
 
     @books << book
     puts "Book '#{title}' by #{author} created successfully" if @books.include?(book)
+  end
+
+  def create_rental
+    if @books.empty?
+      puts 'Please create a book first (option 4).'
+    elsif @people.empty?
+      puts 'Please add a person first (option 3).'
+    else
+      puts 'Select a book from this list by number'
+      @books.each do |book|
+        puts "#{@books.find_index(book)} - #{book.title}"
+      end
+      selected_book = gets.to_i
+      print 'Date: '
+      date = gets.chomp
+      puts 'Select a person from this list by number'
+      @people.each do |person|
+        puts "#{@people.find_index(person)} - #{person.name}"
+      end
+      selected_person = gets.to_i
+
+      rental = Rental.new(date, @books[selected_book], @people[selected_person])
+
+      @rentals << rental
+      puts 'Rental created successfully' if @rentals.include?(rental)
+    end
+  end
+
+  def list_all_rentals_person_id
+    list_all_people
+    print 'Person ID: '
+    person_id = gets.to_i
+    puts(@rentals.map do |rental|
+      if rental.person.id == person_id
+        "Rentals:\nDate: #{rental.date}  Book: #{rental.book.title}"
+      else
+        'No rentals found for this person'
+      end
+    end)
   end
 
 end
--- main.rb
+++ main.rb
@@ -6,7 +6,9 @@ def app_options
   puts '2 - List all people'
   puts '3 - Create a person'
   puts '4 - Create a book'
-  puts '5 - Exit'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
   puts "\n"
 end
 
@@ -20,6 +22,10 @@ def check_selection(response)
     create_person
   when '4'
     create_book
+  when '5'
+    create_rental
+  when '6'
+    list_all_rentals_person_id
   end
 end
 
