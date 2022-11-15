0f09ba988528523fac78a804a29c374da941f0a4
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-15 15:11:39 -0600

    Fix showing ID from file

--- data/user_output.rb
+++ data/user_output.rb
@@ -8,9 +8,11 @@ class UserOutput
     object.each do |person|
       if person['class'] == 'Teacher'
         teacher = Teacher.new(person['age'], person['name'], person['parent_permission'], person['specialization'])
+        teacher.id = person['id']
         people << teacher
       else
         student = Student.new(person['age'], person['name'], person['parent_permission'], person['classroom'])
+        student.id = person['id']
         people << student
       end
     end
